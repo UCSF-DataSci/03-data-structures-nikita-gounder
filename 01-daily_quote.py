6f27e798f93293d82f898e37e0070be60db3a370
Nikita Gounder <116221240+nikita-gounder@users.noreply.github.com>
2024-09-25 23:27:32 +0000

    added quotes

--- 01-daily_quote.py
+++ 01-daily_quote.py
@@ -14,9 +14,13 @@ Hint: Look up `random.choice()` to select a random item from a list. You can use
 import random
 from datetime import date
 
-
+# Create a list of quotes here
 quotes = [
-    # Create a list of quotes here
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+"The way to get started is to quit talking and begin doing. -Walt Disney",
+"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs",
+"The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
+"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey"
 ]
 
 def get_quote_of_the_day(quotes):

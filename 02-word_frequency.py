6716818395c59f306695bba96eb3353271b57079
Nikita Gounder <116221240+nikita-gounder@users.noreply.github.com>
2024-10-02 06:23:47 +0000

    word_freq changes

--- 02-word_frequency.py
+++ 02-word_frequency.py
@@ -5,6 +5,8 @@ Word Frequency Counter
 This script reads a text file and counts the frequency of each word, ignoring case.
 
 Usage: python word_frequency.py <input_file>
+- tar -xzvf alice_in_wonderland.tar.gz #extract txt from gz file
+- python 02-word_frequency.py alice_in_wonderland.txt
 
 
 Your task:
@@ -18,13 +20,20 @@ Hints:
 """
 
 import sys
+import string
 
 def word_frequency(text):
     frequencies = {} # Dictionary to store word frequencies
+    words = text.lower().split() #full text with ignore case + split
 
-    # Your code here
-    
+    for i in words:    # Remove punctuation, whitespace?
+        i = i.strip(string.punctuation)
+        frequencies[i] = frequencies.get(i, 0) + 1
+
+    frequencies = dict(sorted(frequencies.items())[:20]) #ordered and only first 20
+
     return frequencies
+
 
 # Scaffold for opening a file and running word_frequency() on the contents
 if __name__ == "__main__":
@@ -49,3 +58,5 @@ if __name__ == "__main__":
     
     print(f"Word frequencies for '{filename}':")
     print(frequencies)
+
+    
